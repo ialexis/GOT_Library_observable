e804b1f0133ae6bd786509ac93cd4e140a1e7898
Ivan Alexis <ivanalexisabad@gmail.com>
2024-02-12 23:53:22 +0100

    Initial Commit

---
+++ GOT_Library/GOT_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GOT_LibraryApp.swift
+//  GOT_Library
+//
+//  Created by Ivan Alexis Abad on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GOT_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
